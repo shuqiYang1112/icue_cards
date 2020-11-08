39c778f41b9f15a846a76970dc5e1c6a76018ede
shuqiYang1112 <63683638+shuqiYang1112@users.noreply.github.com>
2020-11-08 22:41:52 +0800

    Create main.dart

---
+++ shuqi/main.dart
@@ -0,0 +1,54 @@
+ import 'package:flutter/material.dart';
+import 'headsupstart.dart';
+import 'kahoostart.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Game Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Game Page'),
+        ),
+        body: Center(
+          child: new Column(
+            children: <Widget>[
+              new RaisedButton(
+                child: Text('Heads Up Game'),
+                color: Colors.blueAccent[600],
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HeadsUpStart())),
+              ),
+              new RaisedButton(
+                child: Text('Kahoo Game'),
+                color: Colors.blueAccent[600],
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => KahooStart())),
+              ),
+            ],
+          ),
+        ));
+  }
+}
